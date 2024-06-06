2793eab0d12ae0e122790e322f3cbff569454e26
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-05 18:26:53 -0600

    init

---
+++ astro.config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import lunaria from '@lunariajs/starlight';
+import webmanifest from "astro-webmanifest";
+
+export const locales = {
+  root: {
+    label: 'English',
+    lang: 'en'
+  }
+};
+
+const site = "https://bcuw.dovahkiin.xyz";
+
+// https://astro.build/config
+export default defineConfig({
+  site,
+  integrations: [starlight({
+    favicon: 'favicon.png',
+    title: 'UBCW',
+    social: {
+      github: 'https://github.com/Unofficial-BlossomCraft-Wikis/BCUW'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/Unofficial-BlossomCraft-Wikis/BCUW/edit/main/'
+    },
+    customCss: [
+    // Relative path to your @font-face CSS file.
+    './src/fonts/fonts.css', './src/styles/main.css'],
+    logo: {
+      src: './src/assets/favicon.png'
+    },
+    locales,
+    lastUpdated: true,
+    sidebar: [{
+			label: 'Starting',
+			collapsed: true,
+			items: [
+				{
+					label: "We're migrating to WikiDot",
+					link: '/starter/home/'
+				},
+			],
+    }]
+  }), 
+	webmanifest({
+		name: 'BCUW',
+		icon: './src/assets/favicon.png',
+		short_name: 'BCUW',
+		description: "The BlossomCraft Wiki that's run by players",
+		start_url: '/',
+		theme_color: '#E16FD6',
+		background_color: '#E16FD6',
+		display: 'standalone',
+	}),lunaria({
+		sync: true,
+	}),],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
+});
