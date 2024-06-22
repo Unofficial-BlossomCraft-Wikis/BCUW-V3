0173c7e0dc9b4dedc17fec9ac0b28f899ea672e6
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-22 15:28:52 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import webmanifest from "astro-webmanifest";
-
+import vercel from "@astrojs/vercel/serverless";
 export const locales = {
-  root: { label: 'English', lang: 'en' },
-	es: { label: 'Español', lang: 'es' },
+  root: {
+    label: 'English',
+    lang: 'en'
+  },
+  es: {
+    label: 'Español',
+    lang: 'es'
+  }
 };
-
 const site = "https://bcuw.xyz";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,99 +28,89 @@ export default defineConfig({
     editLink: {
       baseUrl: 'https://github.com/Unofficial-BlossomCraft-Wikis/BCUW/edit/main/'
     },
-    customCss: [
-    './src/styles/main.css',
-    ],
+    customCss: ['./src/styles/main.css'],
     logo: {
       src: './src/assets/favicon.png'
     },
     locales,
     components: {
       Head: './src/components/Head.astro',
-      Sidebar: './src/components/SideBar.astro',
+      Sidebar: './src/components/SideBar.astro'
     },
     lastUpdated: true,
     sidebar: [{
-			label: 'Starting',
-			collapsed: true,
-			items: [
-				{
-					label: "Why and What?",
-					link: '/starter/home/'
-				},
-        {
-          label: "Resources",
-          link: '/starter/resources/'
-        },
-        {
-          label: "Discord Resources",
-          link: '/starter/discordresources/'
-        },
-			],
-    },
-    {
-      label: 'Crates',
-			collapsed: true,
-      items: [
-        {label: 'Season', collapsed: true, autogenerate: { directory: '/crates/season' }},
-      ]
-    },
-    {
-      label: 'Items', 
-      collapsed: true, 
-      autogenerate: { directory: '/items/' }
-    },
-    {
-			label: 'Contributing',
-			collapsed: true,
-			items: [
-        {
-          label: "Staff",
-          link: '/contributing/staff/'
-        },
-				{
-					label: "How to contribute",
-					link: '/contributing/home/'
-				},
-        {
-          label: "i18n Tracker",
-          link: '/contributing/i18n/'
-        },
-			],
+      label: 'Starting',
+      collapsed: true,
+      items: [{
+        label: "Why and What?",
+        link: '/starter/home/'
+      }, {
+        label: "Resources",
+        link: '/starter/resources/'
+      }, {
+        label: "Discord Resources",
+        link: '/starter/discordresources/'
+      }]
     }, {
-			label: 'Usefull things',
-			collapsed: true,
-			items: [
-				{
-					label: "CDN",
-					link: '/usefulthings/cdn/'
-				},
-			],
+      label: 'Crates',
+      collapsed: true,
+      items: [{
+        label: 'Season',
+        collapsed: true,
+        autogenerate: {
+          directory: '/crates/season'
+        }
+      }]
+    }, {
+      label: 'Items',
+      collapsed: true,
+      autogenerate: {
+        directory: '/items/'
+      }
+    }, {
+      label: 'Contributing',
+      collapsed: true,
+      items: [{
+        label: "Staff",
+        link: '/contributing/staff/'
+      }, {
+        label: "How to contribute",
+        link: '/contributing/home/'
+      }, {
+        label: "i18n Tracker",
+        link: '/contributing/i18n/'
+      }]
+    }, {
+      label: 'Usefull things',
+      collapsed: true,
+      items: [{
+        label: "CDN",
+        link: '/usefulthings/cdn/'
+      }]
     }],
-    head: [
-      {
-        tag: 'script',
-        attrs: {
-          src: 'https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js',
-          type: 'module',
-        },
-      },
-    ],
-  }), 
-	webmanifest({
-		name: 'BCUW',
-		icon: './src/assets/favicon.png',
-		short_name: 'BCUW',
-		description: "The BlossomCraft Wiki that's run by players",
-		start_url: '/',
-		theme_color: '#E16FD6',
-		background_color: '#E16FD6',
-		display: 'standalone',
-	}),],
+    head: [{
+      tag: 'script',
+      attrs: {
+        src: 'https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js',
+        type: 'module'
+      }
+    }]
+  }), webmanifest({
+    name: 'BCUW',
+    icon: './src/assets/favicon.png',
+    short_name: 'BCUW',
+    description: "The BlossomCraft Wiki that's run by players",
+    start_url: '/',
+    theme_color: '#E16FD6',
+    background_color: '#E16FD6',
+    display: 'standalone'
+  })],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
